1500b71738608d41fd863c284defa99932e8586a
pkdeimos <praveen.nitk@gmail.com>
2017-11-11 13:44:06 +1100

    Comments added. Errors due to invalid input are captured

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix takes an input matrix and returns a list of function
+##cacheSolve function takes an input matrix and returns its inverse
+
+
+## makeCacheMatrix creates a cache for an input matrix
+## set  --> Sets the value of matrix
+## get  --> Retrieves the value of x
+## setinv --> sets the inverse of matrix for caching
+## getinv --> Returns the cached value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     invx <- NULL
@@ -24,13 +30,26 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a cached matrix object and returns its inverse. 
+## It checks if inverse is already calculated and stored in the cache
+## Returns cached inverse matrix if available
+## else, calculates inverse, stores in cache and returns it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    if(class(x$get())!="matrix")
+    {
+        print("Input is not a valid matrix!")
+        return(-1)
+    }
+    if(ncol(x$get())!=nrow(x$get()))
+    {
+        print("Input matrix is not inversible. Input should be a square matrix!")
+        return(-1)
+    }
     invx <- x$getinv()
     if(!is.null(invx)) {
-        message("getting cached data")
+        message("getting cached data 1")
         return(invx)
     }
     data <- x$get()
